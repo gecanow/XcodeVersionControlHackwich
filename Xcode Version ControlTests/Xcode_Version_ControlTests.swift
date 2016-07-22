f361b19ffc9b2e4daec3c67de51b647feb2ea6fb
Gaby Ecanow <marcifirfer@loulies-macbook-pro.local>
2016-07-22 10:14:48 -0500

    Initial Commit

---
+++ Xcode Version ControlTests/Xcode_Version_ControlTests.swift
@@ -0,0 +1,36 @@
+//
+//  Xcode_Version_ControlTests.swift
+//  Xcode Version ControlTests
+//
+//  Created by Gaby Ecanow on 7/22/16.
+//  Copyright © 2016 Gaby Ecanow. All rights reserved.
+//
+
+import XCTest
+@testable import Xcode_Version_Control
+
+class Xcode_Version_ControlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
